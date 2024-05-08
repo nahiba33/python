640f98f85e99a26351edfc1cb8fcb92989baf58f
nahiba33 <161586401+nahiba33@users.noreply.github.com>
2024-05-08 22:25:31 +0400

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -5,7 +5,7 @@ class Şahmat():
         print("Şahmat yaradıldı")
     def hərəkət(self, yenixana):
         pass
-class Qala(Şahmat):
+class Top(Şahmat):
     def __init__(self, rəng, xana):
      super().__init__(rəng, xana)
     def hərəkət(self, yenixana):
@@ -13,9 +13,9 @@ class Qala(Şahmat):
         x1, y1 = yenixana
         if x1 == x or y1 == y:
             self.xana = yenixana
-            print("Qala üfüqi yaxud da şaquli hərəkət etdi")
+            print("Top üfüqi yaxud da şaquli hərəkət etdi")
         else:
-            print("Qala belə hərəkət edə bilmir")
-qala = Qala("Qara", (0, 0))
-qala.hərəkət((4, 0))
-qala.hərəkət((0,1))
+            print("Top belə hərəkət edə bilmir")
+top = Top("Qara", (0, 0))
+top.hərəkət((4, 0))
+top.hərəkət((0,1))
